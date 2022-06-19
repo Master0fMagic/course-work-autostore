33b348f68e2b008b979bf0844160d53f167e55c2
Max <56440333+s1maxx@users.noreply.github.com>
2022-06-19 15:45:43 +0300

    add react app

---
+++ ui/src/redux/store/user/slice.ts
@@ -0,0 +1,50 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {GetOrder, PostLogin} from "../../../api/Api";
+
+export enum Statuses {
+    none = "none",
+    waiting = "waiting",
+    success = "success",
+    error = "error"
+}
+
+
+export interface UserState {
+    isAuth: boolean,
+    account: User
+    errorMsg: string
+}
+
+const INITIAL_STATE = {
+    account: {} as User,
+    isAuth: false,
+    errorMsg: ""
+};
+
+interface User{
+    login: string,
+    password: string
+}
+
+const userSlice = createSlice({
+    name: "user",
+    initialState: INITIAL_STATE,
+    reducers: {
+        login(state, action:PayloadAction<PostLogin>) {
+            state.account = {} as User;
+            state.isAuth = false;
+        },
+        setAuth: (state, action) => {
+            state.isAuth = action.payload;
+        },
+        setUser: (state, action) => {
+            state.account = action.payload;
+        },
+        logout: (state) => {
+            console.log("logout")
+        },
+    }
+})
+
+export default userSlice.reducer;
+export const { setUser, setAuth, login, logout } = userSlice.actions;
